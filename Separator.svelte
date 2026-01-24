a036a013002c4140b10fb9a9232ac6e21ad193f4
Henning Storck <mail@henningstorck.com>
2026-01-24 14:52:31 +0100

    Add separator component

---
+++ Separator.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	interface Props {
+		title?: string;
+	}
+
+	let { title }: Props = $props();
+</script>
+
+<div class="separator">{title}</div>
+
+<style>
+	:where(.separator) {
+		--separator-margin: var(--size-5);
+		--separator-fg: var(--surface-4);
+	}
+
+	.separator {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: var(--size-3);
+		margin-block: var(--separator-margin);
+		text-align: center;
+		color: var(--separator-fg);
+		text-transform: uppercase;
+
+		&:before,
+		&:after {
+			display: block;
+			content: '';
+			height: var(--border-size-2);
+			background-color: var(--separator-fg);
+			flex: 1 1 0;
+		}
+	}
+</style>
