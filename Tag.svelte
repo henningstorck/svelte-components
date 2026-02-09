7a084e208c80daee648eeea9ff1e4c28440e2f50
Henning Storck <mail@henningstorck.com>
2026-02-09 06:32:10 +0100

    Add tag and tag list components

---
+++ Tag.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		children: Snippet;
+		color: string;
+	}
+
+	let { children, color }: Props = $props();
+</script>
+
+<div class="tag" style="--tag-color: {color}">
+	{@render children()}
+</div>
+
+<style>
+	.tag {
+		display: flex;
+		align-items: center;
+		gap: var(--size-2);
+		font-size: var(--font-size-0);
+		text-transform: uppercase;
+
+		&::before {
+			content: '';
+			display: inline-block;
+			width: var(--size-3);
+			height: var(--size-2);
+			background-color: var(--tag-color);
+			border-radius: var(--radius-round);
+		}
+	}
+</style>
---
+++ TagList.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+<div class="tag-list">
+	{@render children()}
+</div>
+
+<style>
+	.tag-list {
+		display: flex;
+		flex-wrap: wrap;
+		gap: var(--size-2) var(--size-4);
+	}
+</style>
