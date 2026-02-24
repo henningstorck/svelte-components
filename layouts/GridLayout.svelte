54721746793f855dfc7fb00df8c487c293025333
Henning Storck <mail@henningstorck.com>
2026-02-24 16:26:35 +0100

    Add property to set grid layout stretch

--- layouts/GridLayout.svelte
+++ layouts/GridLayout.svelte
@@ -4,12 +4,13 @@
 	interface Props {
 		children: Snippet;
 		minWidth?: number;
+		stretch?: boolean;
 	}
 
-	let { children, minWidth = 400 }: Props = $props();
+	let { children, minWidth = 400, stretch = false }: Props = $props();
 </script>
 
-<div class="grid-layout" style="--grid-layout-min-width: {minWidth}px">
+<div class="grid-layout" style="--grid-layout-min-width: {minWidth}px" class:stretch>
 	{@render children()}
 </div>
 
@@ -22,8 +23,15 @@
 		display: grid;
 		gap: var(--grid-layout-gap);
 		grid-template-columns: repeat(
-			auto-fit,
+			auto-fill,
 			minmax(min(var(--grid-layout-min-width), 100%), 1fr)
 		);
+
+		&.stretch {
+			grid-template-columns: repeat(
+				auto-fit,
+				minmax(min(var(--grid-layout-min-width), 100%), 1fr)
+			);
+		}
 	}
 </style>
